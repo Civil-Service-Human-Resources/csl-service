274279abbb8a43bd5dbe5f983717f169b6c25ffa
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2022-11-29 12:39:49 +0000

    LC-1416 - Initial draft wip code

---
+++ src/test/java/uk/gov/cabinetoffice/csl/cslservice/CslServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.cabinetoffice.csl.cslservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CslServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
