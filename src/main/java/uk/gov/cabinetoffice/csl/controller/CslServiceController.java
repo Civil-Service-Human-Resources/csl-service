935936cee11a7a6744d0bb3069ae18e7fd678eaf
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2022-12-06 12:58:03 +0000

    LC-1416 - Postman collection updated and a controller added

---
+++ src/main/java/uk/gov/cabinetoffice/csl/controller/CslServiceController.java
@@ -0,0 +1,20 @@
+package uk.gov.cabinetoffice.csl.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@Slf4j
+@RestController
+public class CslServiceController {
+
+    @GetMapping(path = "/test/{input}")
+    public ResponseEntity<String> test(@PathVariable("input") String input) {
+        log.info("Input, {}", input);
+        return new ResponseEntity<>(input, OK);
+    }
+}
