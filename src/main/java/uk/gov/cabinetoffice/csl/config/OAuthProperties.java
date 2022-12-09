aa1f8033b069c6f4809d25a10c4482260e0c4fc0
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2022-12-09 13:44:37 +0000

    LC-1416 - Implemneting OAuth2 as per the existing code

---
+++ src/main/java/uk/gov/cabinetoffice/csl/config/OAuthProperties.java
@@ -0,0 +1,30 @@
+package uk.gov.cabinetoffice.csl.config;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.Serializable;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "oauth")
+public class OAuthProperties implements Serializable {
+
+    private String serviceUrl;
+
+    private String clientId;
+
+    private String clientSecret;
+
+    private String tokenUrl;
+
+    private String checkTokenUrl;
+
+    private int maxTotalConnections;
+
+    private int defaultMaxConnectionsPerRoute;
+
+    private int maxPerServiceUrl;
+
+    private String jwtKey;
+}
---
+++ src/main/java/uk/gov/cabinetoffice/csl/config/SecurityConfig.java
@@ -0,0 +1,101 @@
+package uk.gov.cabinetoffice.csl.config;
+
+
+import org.apache.http.HttpHost;
+import org.apache.http.conn.routing.HttpRoute;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.AccessTokenRequest;
+import org.springframework.security.oauth2.client.token.DefaultAccessTokenRequest;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@EnableResourceServer
+@EnableWebSecurity
+@EnableOAuth2Client
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/health").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    public void configure(WebSecurity webSecurity) throws Exception{
+        webSecurity.ignoring().antMatchers(HttpMethod.GET, "/health");
+    }
+
+    @Bean
+    public TokenStore getTokenStore(OAuthProperties oAuthProperties) {
+        return new JwtTokenStore(accessTokenConverter(oAuthProperties));
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter(OAuthProperties oAuthProperties) {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setSigningKey(oAuthProperties.getJwtKey());
+        return jwtAccessTokenConverter;
+    }
+
+    @Bean
+    public OAuth2ProtectedResourceDetails resourceDetails(OAuthProperties oAuthProperties) {
+        ClientCredentialsResourceDetails resource = new ClientCredentialsResourceDetails();
+        resource.setId("identity");
+        resource.setAccessTokenUri(oAuthProperties.getTokenUrl());
+        resource.setClientId(oAuthProperties.getClientId());
+        resource.setClientSecret(oAuthProperties.getClientSecret());
+        return resource;
+    }
+
+    @Bean
+    public PoolingHttpClientConnectionManager httpClientConnectionManager(OAuthProperties oAuthProperties) {
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(oAuthProperties.getMaxTotalConnections());
+        connectionManager.setDefaultMaxPerRoute(oAuthProperties.getDefaultMaxConnectionsPerRoute());
+        HttpHost host = new HttpHost(oAuthProperties.getServiceUrl());
+        connectionManager.setMaxPerRoute(new HttpRoute(host), oAuthProperties.getMaxPerServiceUrl());
+        return connectionManager;
+    }
+
+    @Bean
+    public OAuth2RestOperations oAuthRestTemplate(OAuth2ProtectedResourceDetails resourceDetails, PoolingHttpClientConnectionManager connectionManager) {
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+                .setConnectionManager(connectionManager)
+                .build();
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+        AccessTokenRequest atr = new DefaultAccessTokenRequest();
+        OAuth2RestTemplate oAuthRestTemplate = new OAuth2RestTemplate(resourceDetails, new DefaultOAuth2ClientContext(atr));
+        oAuthRestTemplate.setRequestFactory(requestFactory);
+        return oAuthRestTemplate;
+    }
+}
+
