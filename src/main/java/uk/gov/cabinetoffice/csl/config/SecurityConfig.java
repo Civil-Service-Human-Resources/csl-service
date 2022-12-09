bb8bb1841b4c7341ae58598b3a8d56943b34f442
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2022-12-09 14:44:47 +0000

    LC-1591 - Logs added

--- src/main/java/uk/gov/cabinetoffice/csl/config/SecurityConfig.java
+++ src/main/java/uk/gov/cabinetoffice/csl/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package uk.gov.cabinetoffice.csl.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHost;
 import org.apache.http.conn.routing.HttpRoute;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
@@ -32,54 +33,70 @@ import org.springframework.web.client.RestTemplate;
 @EnableResourceServer
 @EnableWebSecurity
 @EnableOAuth2Client
+@Slf4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        log.debug("configure(HttpSecurity http): http: {}", http.toString());
         http.csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/health").permitAll()
                 .anyRequest().authenticated();
+        log.debug("configure(HttpSecurity http): End");
     }
 
     @Override
     public void configure(WebSecurity webSecurity) throws Exception{
+        log.debug("configure(WebSecurity webSecurity): webSecurity: {}", webSecurity.toString());
         webSecurity.ignoring().antMatchers(HttpMethod.GET, "/health");
+        log.debug("configure(WebSecurity webSecurity): webSecurity: End");
     }
 
     @Bean
     public TokenStore getTokenStore(OAuthProperties oAuthProperties) {
-        return new JwtTokenStore(accessTokenConverter(oAuthProperties));
+        log.debug("getTokenStore: oAuthProperties: {}", oAuthProperties.toString());
+        JwtTokenStore jwtTokenStore = new JwtTokenStore(accessTokenConverter(oAuthProperties));
+        log.debug("getTokenStore: jwtTokenStore: {}", oAuthProperties.toString());
+        return jwtTokenStore;
     }
 
     @Bean
     public JwtAccessTokenConverter accessTokenConverter(OAuthProperties oAuthProperties) {
+        log.debug("accessTokenConverter: oAuthProperties: {}", oAuthProperties.toString());
         JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
         jwtAccessTokenConverter.setSigningKey(oAuthProperties.getJwtKey());
+        log.debug("accessTokenConverter: jwtAccessTokenConverter: {}", jwtAccessTokenConverter.toString());
         return jwtAccessTokenConverter;
     }
 
     @Bean
     public OAuth2ProtectedResourceDetails resourceDetails(OAuthProperties oAuthProperties) {
+        log.debug("resourceDetails: oAuthProperties: {}", oAuthProperties.toString());
         ClientCredentialsResourceDetails resource = new ClientCredentialsResourceDetails();
         resource.setId("identity");
         resource.setAccessTokenUri(oAuthProperties.getTokenUrl());
         resource.setClientId(oAuthProperties.getClientId());
         resource.setClientSecret(oAuthProperties.getClientSecret());
+        log.debug("resourceDetails: resource: {}", resource.toString());
         return resource;
     }
 
     @Bean
     public PoolingHttpClientConnectionManager httpClientConnectionManager(OAuthProperties oAuthProperties) {
+        log.debug("httpClientConnectionManager: oAuthProperties: {}", oAuthProperties.toString());
         PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
         connectionManager.setMaxTotal(oAuthProperties.getMaxTotalConnections());
         connectionManager.setDefaultMaxPerRoute(oAuthProperties.getDefaultMaxConnectionsPerRoute());
         HttpHost host = new HttpHost(oAuthProperties.getServiceUrl());
         connectionManager.setMaxPerRoute(new HttpRoute(host), oAuthProperties.getMaxPerServiceUrl());
+        log.debug("httpClientConnectionManager: connectionManager: {}", connectionManager.toString());
         return connectionManager;
     }
 
     @Bean
     public OAuth2RestOperations oAuthRestTemplate(OAuth2ProtectedResourceDetails resourceDetails, PoolingHttpClientConnectionManager connectionManager) {
+        log.debug("oAuthRestTemplate: resourceDetails: {}", resourceDetails.toString());
+        log.debug("oAuthRestTemplate: connectionManager: {}", connectionManager.toString());
         CloseableHttpClient httpClient = HttpClients.custom()
                 .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
                 .setConnectionManager(connectionManager)
@@ -89,6 +106,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         AccessTokenRequest atr = new DefaultAccessTokenRequest();
         OAuth2RestTemplate oAuthRestTemplate = new OAuth2RestTemplate(resourceDetails, new DefaultOAuth2ClientContext(atr));
         oAuthRestTemplate.setRequestFactory(requestFactory);
+        log.debug("oAuthRestTemplate: oAuthRestTemplate: {}", oAuthRestTemplate.toString());
         return oAuthRestTemplate;
     }
 
