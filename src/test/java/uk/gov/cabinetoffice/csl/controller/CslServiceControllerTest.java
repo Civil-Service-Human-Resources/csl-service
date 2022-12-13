0a5202b2032535735d0a7e384cc482f9d3c16a73
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2022-12-13 00:07:17 +0000

    LC-1591 - Security disbaled in unit tests

--- src/test/java/uk/gov/cabinetoffice/csl/controller/CslServiceControllerTest.java
+++ src/test/java/uk/gov/cabinetoffice/csl/controller/CslServiceControllerTest.java
@@ -2,24 +2,27 @@ package uk.gov.cabinetoffice.csl.controller;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(CslServiceController.class)
+@WebMvcTest(controllers = CslServiceController.class, excludeAutoConfiguration = {SecurityAutoConfiguration.class})
+@AutoConfigureMockMvc(addFilters = false)
+@RunWith(SpringRunner.class)
+//@AutoConfigureMockMvc(secure = false)
 public class CslServiceControllerTest {
-
     @Autowired
     private MockMvc mockMvc;
 
@@ -33,7 +36,7 @@ public class CslServiceControllerTest {
 
     MockHttpServletRequestBuilder buildGet(String input) {
         return get("/csl/test/" + input)
-                //.with(csrf())
+                .with(csrf())
                 .contentType(MediaType.APPLICATION_JSON);
     }
 }
