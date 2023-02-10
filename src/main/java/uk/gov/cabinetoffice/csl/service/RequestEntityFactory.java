ab707e04edc4e8c34d6d2c177285be80e2d85316
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2023-02-10 13:54:47 +0000

    LC-1501 CSL-Service Launch e-Learning

--- src/main/java/uk/gov/cabinetoffice/csl/service/RequestEntityFactory.java
+++ src/main/java/uk/gov/cabinetoffice/csl/service/RequestEntityFactory.java
@@ -1,17 +1,13 @@
 package uk.gov.cabinetoffice.csl.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.tomcat.util.codec.binary.Base64;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.http.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -21,113 +17,91 @@ import java.util.List;
 @Component
 public class RequestEntityFactory {
 
-    @Value("${oauth.clientId}")
-    private String clientId;
-
-    @Value("${oauth.clientSecret}")
-    private String clientSecret;
-
-    @Value("${oauth.tokenUrl}")
-    private String clientUrl;
-
-    public RequestEntity<?> createGetRequest(String strUri) {
+    public RequestEntity<?> createGetRequestWithBearerToken(String strUri) {
         try {
             URI uri = UriComponentsBuilder.fromUriString(strUri)
                     .build()
                     .toUri();
-            return createGetRequest(uri);
+            return createGetRequestWithBearerToken(uri);
         } catch (JSONException e) {
             throw new RequestEntityException(e);
         }
     }
 
-    public RequestEntity<?> createGetRequest(URI uri) throws JSONException {
-        HttpHeaders headers = createHttpHeaders();
+    public RequestEntity<?> createGetRequestWithBearerToken(URI uri) throws JSONException {
+        HttpHeaders headers = createHttpHeadersWithBearerToken();
         return RequestEntity.get(uri).headers(headers).build();
     }
 
-    public RequestEntity<?> createPostRequest(String strUri, Object body) {
+    public RequestEntity<?> createPostRequestWithBearerToken(String strUri, Object body) {
         try {
             URI uri = UriComponentsBuilder.fromUriString(strUri)
                     .build()
                     .toUri();
-            return createPostRequest(uri, body);
+            return createPostRequestWithBearerToken(uri, body);
         } catch (JSONException e) {
             throw new RequestEntityException(e);
         }
     }
 
-    public RequestEntity<?> createPostRequest(URI uri, Object body) throws JSONException {
-        HttpHeaders headers = createHttpHeaders();
+    public RequestEntity<?> createPostRequestWithBearerToken(URI uri, Object body) throws JSONException {
+        HttpHeaders headers = createHttpHeadersWithBearerToken();
         return RequestEntity.post(uri).headers(headers).body(body);
     }
 
-    private HttpHeaders createHttpHeaders() throws JSONException {
-        String clientAccessToken = getClientAccessTokenFromSecurityContext();
-        if(StringUtils.isBlank(clientAccessToken)) {
-            clientAccessToken = getClientAccessTokenFromCache();
-            if(StringUtils.isBlank(clientAccessToken)) {
-                clientAccessToken = getClientAccessTokenFromIdentityService();
-            }
-        }
-        log.debug("Token Value: {}", clientAccessToken);
+    private HttpHeaders createHttpHeadersWithBearerToken() throws JSONException {
+        String bearerToken = getBearerTokenFromSecurityContext();
+        log.debug("Bearer Token Value: {}", bearerToken);
         HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + clientAccessToken);
+        headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + bearerToken);
         return headers;
     }
 
-    private String getClientAccessTokenFromSecurityContext() {
+    private String getBearerTokenFromSecurityContext() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
-        log.debug("Authenticated?: {}", authentication.isAuthenticated());
-        log.debug("Authentication jwtPrincipal: {}", jwtPrincipal);
-        log.debug("Authentication jwtPrincipal Claims: {}", jwtPrincipal.getClaims());
-        log.debug("Authentication jwtPrincipal Headers: {}",  jwtPrincipal.getHeaders());
-        log.debug("Authentication jwtPrincipal ExpiresAt: {}", jwtPrincipal.getExpiresAt());
-        log.debug("Authentication jwtPrincipal Id: {}", jwtPrincipal.getId());
-        log.debug("Authentication jwtPrincipal IssuedAt: {}", jwtPrincipal.getIssuedAt());
-        log.debug("Authentication jwtPrincipal TokenValue: {}", jwtPrincipal.getTokenValue());
-        String tokenValue = jwtPrincipal.getTokenValue();
-        //TODO: check if token is client type and not expired then return it else return null
-        //isTokenExpired(tokenValue);
-        return tokenValue;
+        return jwtPrincipal.getTokenValue();
     }
 
-    private String getClientAccessTokenFromCache() {
-        //TODO: Retrieve token from cache and check if token is not expired then return it else return null
-        //isTokenExpired(tokenValue);
-        return null;
+    public RequestEntity<?> createGetRequestWithCredentials(String strUri, String apiUsername, String apiPassword) {
+        try {
+            URI uri = UriComponentsBuilder.fromUriString(strUri)
+                    .build()
+                    .toUri();
+            return createGetRequestWithCredentials(uri, apiUsername, apiPassword);
+        } catch (JSONException e) {
+            throw new RequestEntityException(e);
+        }
     }
 
-    private String getClientAccessTokenFromIdentityService() throws JSONException {
-        RestTemplate restTemplate = new RestTemplate();
+    public RequestEntity<?> createGetRequestWithCredentials(URI uri, String apiUsername, String apiPassword) throws JSONException {
+        HttpHeaders headers = createHttpHeadersWithCredentials(apiUsername, apiPassword);
+        return RequestEntity.get(uri).headers(headers).build();
+    }
 
-        String credentials = clientId + ":" + clientSecret;
-        String encodedCredentials = new String(Base64.encodeBase64(credentials.getBytes(), false));
+    public RequestEntity<?> createPostRequestWithCredentials(String strUri, Object body, String apiUsername, String apiPassword) {
+        try {
+            URI uri = UriComponentsBuilder.fromUriString(strUri)
+                    .build()
+                    .toUri();
+            return createPostRequestWithCredentials(uri, body, apiUsername, apiPassword);
+        } catch (JSONException e) {
+            throw new RequestEntityException(e);
+        }
+    }
+
+    public RequestEntity<?> createPostRequestWithCredentials(URI uri, Object body, String apiUsername, String apiPassword) throws JSONException {
+        HttpHeaders headers = createHttpHeadersWithCredentials(apiUsername, apiPassword);
+        return RequestEntity.post(uri).headers(headers).body(body);
+    }
+
+    private HttpHeaders createHttpHeadersWithCredentials(String apiUsername, String apiPassword) throws JSONException {
+        String apiCredentials = apiUsername + ":" + apiPassword;
+        String encodedApiCredentials = new String(Base64.encodeBase64(apiCredentials.getBytes(), false));
 
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(List.of(MediaType.APPLICATION_JSON));
-        headers.add("Authorization", "Basic " + encodedCredentials);
-
-        HttpEntity<String> request = new HttpEntity<String>(headers);
-
-        String access_token_url = clientUrl;
-        access_token_url += "?grant_type=client_credentials";
-
-        ResponseEntity<String> response = restTemplate.exchange(access_token_url, HttpMethod.POST, request, String.class);
-        JSONObject jsonObject = new JSONObject(response.getBody());
-        String tokenValue = jsonObject.getString("access_token");
-        cacheClientAccessToken(tokenValue);
-        //TODO: Write code to decrypt the tokenValue for debugging
-        return tokenValue;
-    }
-
-    private void cacheClientAccessToken(String tokenValue) {
-        //TODO: Implement this
-    }
-
-    private boolean isTokenExpired(String expiryDate) {
-        //TODO: Implement this
-        return false;
+        headers.add("Authorization", "Basic " + encodedApiCredentials);
+        return headers;
     }
 }
