de4c5ca5e658517ed3ad07227ce1e00ca9b9dbec
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2024-05-24 11:22:17 +0100

    Serialize course response to list rather than arraylist

--- src/main/java/uk/gov/cabinetoffice/csl/util/CacheGetMultipleOp.java
+++ src/main/java/uk/gov/cabinetoffice/csl/util/CacheGetMultipleOp.java
@@ -3,12 +3,11 @@ package uk.gov.cabinetoffice.csl.util;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Data
 public class CacheGetMultipleOp<T extends Cacheable> {
     private final List<String> cacheMisses;
-    private final ArrayList<T> cacheHits;
+    private final List<T> cacheHits;
 }
