7ad4a0f5575d1f6b71c82813d90b5769bf5b3040
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2024-08-01 12:11:26 +0100

    Include empty values and timezone recognition in chart builder logic

--- src/main/java/uk/gov/cabinetoffice/csl/service/ChartFactory.java
+++ src/main/java/uk/gov/cabinetoffice/csl/service/ChartFactory.java
@@ -87,47 +87,4 @@ public class ChartFactory {
                 params.getBinDelimiter(), hasRequests);
     }
 
-//    public CourseCompletionChart buildCourseCompletionsChart(GetCourseCompletionsParams params,
-//                                                             AggregationResponse<CourseCompletionAggregation> aggregations,
-//                                                             boolean hasRequests) {
-//        Map<String, String> courseIdToTitleMap = learningCatalogueService.getCourses(params.getCourseIds())
-//                .stream().collect(Collectors.toMap(Course::getId, Course::getTitle));
-//        Map<String, Integer> totalResults = new LinkedHashMap<>();
-//        Map<String, Integer> courseIdBreakdown = new HashMap<>();
-//        Integer courseSummaryTotal = 0;
-//        for (CourseCompletionAggregation result : aggregations.getResults()) {
-//
-//            courseSummaryTotal += result.getTotal();
-//            String courseId = result.getCourseId();
-//            Integer courseTotal = courseIdBreakdown.get(courseId);
-//            if (courseTotal == null) {
-//                courseTotal = result.getTotal();
-//            } else {
-//                courseTotal = courseTotal + result.getTotal();
-//            }
-//            courseIdBreakdown.put(courseId, courseTotal);
-//
-//            String stringedDateTime = result.getDateBin().format(DateTimeFormatter.ISO_DATE_TIME);
-//            Integer count = totalResults.get(stringedDateTime);
-//            if (count == null) {
-//                count = result.getTotal();
-//            } else {
-//                count = count + result.getTotal();
-//            }
-//            totalResults.put(stringedDateTime, count);
-//        }
-//
-//        Map<String, Integer> courseTitleBreakdown = new HashMap<>();
-//        courseIdToTitleMap.forEach((id, title) -> {
-//            Integer countFromBreakdown = courseIdBreakdown.get(id);
-//            Integer count = countFromBreakdown == null ? 0 : countFromBreakdown;
-//            courseTitleBreakdown.put(title, count);
-//        });
-//
-//        List<PlotPoint> plotPoints = totalResults.entrySet().stream()
-//                .map(e -> new PlotPoint(e.getKey(), e.getValue())).toList();
-//        return new CourseCompletionChart(plotPoints, courseTitleBreakdown, params.getTimezone(), courseSummaryTotal,
-//                params.getBinDelimiter().getVal(), hasRequests);
-//    }
-
 }
