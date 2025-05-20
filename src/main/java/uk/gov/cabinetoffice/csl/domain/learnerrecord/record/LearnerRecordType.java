cc983b96c5d9bf26bc153eea9c3acea954f3cc3e
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2025-05-20 15:31:35 +0100

    Serialize learner record type

--- src/main/java/uk/gov/cabinetoffice/csl/domain/learnerrecord/record/LearnerRecordType.java
+++ src/main/java/uk/gov/cabinetoffice/csl/domain/learnerrecord/record/LearnerRecordType.java
@@ -5,10 +5,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.cabinetoffice.csl.domain.LearningResourceType;
 
+import java.io.Serializable;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class LearnerRecordType {
+public class LearnerRecordType implements Serializable {
     private Integer id;
     private String type;
 
