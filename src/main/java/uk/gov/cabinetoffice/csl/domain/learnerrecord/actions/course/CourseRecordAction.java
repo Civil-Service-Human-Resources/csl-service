d017fd4c4a252df7e27b515917e2e1b70e79bdf5
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2025-05-22 10:12:08 +0100

    Allow remove from learning plan to repeat

--- src/main/java/uk/gov/cabinetoffice/csl/domain/learnerrecord/actions/course/CourseRecordAction.java
+++ src/main/java/uk/gov/cabinetoffice/csl/domain/learnerrecord/actions/course/CourseRecordAction.java
@@ -3,11 +3,14 @@ package uk.gov.cabinetoffice.csl.domain.learnerrecord.actions.course;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * There is scope here to extract the properties out into spring configuration classes, but this is enough for now
+ */
 @Getter
 @RequiredArgsConstructor
 public enum CourseRecordAction implements ICourseRecordActionType {
     MOVE_TO_LEARNING_PLAN("MOVE_TO_LEARNING_PLAN", "Move to learning plan", true, false),
-    REMOVE_FROM_LEARNING_PLAN("REMOVE_FROM_LEARNING_PLAN", "Remove from learning plan", false, false),
+    REMOVE_FROM_LEARNING_PLAN("REMOVE_FROM_LEARNING_PLAN", "Remove from learning plan", false, true),
     REMOVE_FROM_SUGGESTIONS("REMOVE_FROM_SUGGESTIONS", "Remove from suggestions", true, false),
     COMPLETE_COURSE("COMPLETE_COURSE", "Complete a course", true, true);
 
