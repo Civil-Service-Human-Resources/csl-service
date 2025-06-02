dd44da1d64926805a000d6c965394e3af4e73457
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2025-06-02 10:05:48 +0100

    LC-3465 - GET organisations formatted list

--- src/main/java/uk/gov/cabinetoffice/csl/client/csrs/CSRSClient.java
+++ src/main/java/uk/gov/cabinetoffice/csl/client/csrs/CSRSClient.java
@@ -3,10 +3,14 @@ package uk.gov.cabinetoffice.csl.client.csrs;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Component;
 import uk.gov.cabinetoffice.csl.client.IHttpClient;
 import uk.gov.cabinetoffice.csl.domain.csrs.CivilServant;
+import uk.gov.cabinetoffice.csl.domain.csrs.OrganisationalUnit;
+
+import java.util.List;
 
 @Component
 @Slf4j
@@ -14,6 +18,9 @@ public class CSRSClient implements ICSRSClient {
 
     @Value("${csrs.civilServants}")
     private String civilServants;
+
+    @Value("${csrs.allOrganisationalUnits}")
+    private String allOrganisationalUnits;
 
     private final IHttpClient httpClient;
 
@@ -27,5 +34,13 @@ public class CSRSClient implements ICSRSClient {
         String url = String.format("%s/resource/%s/profile", civilServants, uid);
         RequestEntity<Void> request = RequestEntity.get(url).build();
         return httpClient.executeRequest(request, CivilServant.class);
+    }
+
+    @Override
+    public List<OrganisationalUnit> getAllOrganisationalUnits() {
+        log.info("Getting all organisational units");
+        RequestEntity<Void> request = RequestEntity.get(allOrganisationalUnits).build();
+        return httpClient.executeTypeReferenceRequest(request, new ParameterizedTypeReference<>() {
+        });
     }
 }
--- src/main/java/uk/gov/cabinetoffice/csl/client/csrs/ICSRSClient.java
+++ src/main/java/uk/gov/cabinetoffice/csl/client/csrs/ICSRSClient.java
@@ -1,8 +1,13 @@
 package uk.gov.cabinetoffice.csl.client.csrs;
 
 import uk.gov.cabinetoffice.csl.domain.csrs.CivilServant;
+import uk.gov.cabinetoffice.csl.domain.csrs.OrganisationalUnit;
+
+import java.util.List;
 
 public interface ICSRSClient {
 
     CivilServant getCivilServantProfileWithUid(String uid);
+
+    List<OrganisationalUnit> getAllOrganisationalUnits();
 }
