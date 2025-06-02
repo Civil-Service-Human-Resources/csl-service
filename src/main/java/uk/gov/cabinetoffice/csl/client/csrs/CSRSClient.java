3ae4ce593876e24f899bbfe804c0cdbf9947e55c
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2025-06-02 12:58:35 +0100

    LC-3465 - GET organisations formatted list

--- src/main/java/uk/gov/cabinetoffice/csl/client/csrs/CSRSClient.java
+++ src/main/java/uk/gov/cabinetoffice/csl/client/csrs/CSRSClient.java
@@ -3,18 +3,24 @@ package uk.gov.cabinetoffice.csl.client.csrs;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 import uk.gov.cabinetoffice.csl.client.IHttpClient;
+import uk.gov.cabinetoffice.csl.client.model.PagedResponse;
 import uk.gov.cabinetoffice.csl.domain.csrs.CivilServant;
 import uk.gov.cabinetoffice.csl.domain.csrs.OrganisationalUnit;
+import uk.gov.cabinetoffice.csl.domain.csrs.record.OrganisationalUnitsPagedResponse;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 @Slf4j
 public class CSRSClient implements ICSRSClient {
+
+    @Value("${csrs.organisationalUnitMaxPageSize}")
+    private Integer organisationalUnitMaxPageSize;
 
     @Value("${csrs.civilServants}")
     private String civilServants;
@@ -39,8 +45,21 @@ public class CSRSClient implements ICSRSClient {
     @Override
     public List<OrganisationalUnit> getAllOrganisationalUnits() {
         log.info("Getting all organisational units");
-        RequestEntity<Void> request = RequestEntity.get(allOrganisationalUnits).build();
-        return httpClient.executeTypeReferenceRequest(request, new ParameterizedTypeReference<>() {
-        });
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromPath(allOrganisationalUnits);
+        return getPaginatedRequest(OrganisationalUnitsPagedResponse.class, uriBuilder, organisationalUnitMaxPageSize)
+                .stream().toList();
+    }
+
+    private <T, R extends PagedResponse<T>> List<T> getPaginatedRequest(Class<R> pagedResponseClass, UriComponentsBuilder url, Integer maxPageSize) {
+        List<T> results = new ArrayList<>();
+        int totalPages = 1;
+        for (int i = 0; i < totalPages; i++) {
+            url.queryParam("page", i).queryParam("size", maxPageSize);
+            RequestEntity<Void> request = RequestEntity.get(url.build().toUriString()).build();
+            R response = httpClient.executeRequest(request, pagedResponseClass);
+            results.addAll(response.getContent());
+            totalPages = response.getTotalPages();
+        }
+        return results;
     }
 }
