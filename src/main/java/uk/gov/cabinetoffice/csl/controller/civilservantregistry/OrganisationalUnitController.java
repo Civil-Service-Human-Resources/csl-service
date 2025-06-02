ffbba45833a575461ba78447d041d7cff4f7313b
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2025-06-02 15:15:01 +0100

    LC-3465 - GET organisations formatted list

--- src/main/java/uk/gov/cabinetoffice/csl/controller/civilservantregistry/OrganisationalUnitController.java
+++ src/main/java/uk/gov/cabinetoffice/csl/controller/civilservantregistry/OrganisationalUnitController.java
@@ -1,6 +1,8 @@
 package uk.gov.cabinetoffice.csl.controller.civilservantregistry;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import uk.gov.cabinetoffice.csl.domain.csrs.OrganisationalUnit;
 import uk.gov.cabinetoffice.csl.service.civilservantregistry.CivilServantRegistryService;
@@ -18,7 +20,9 @@ public class OrganisationalUnitController {
     }
 
     @GetMapping("/v2/organisationalUnits")
+    @PreAuthorize("isAuthenticated()")
     @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
     public List<OrganisationalUnit> getAllOrganisationalUnits() {
         log.info("Getting all organisational units");
         return civilServantRegistryService.getAllOrganisationalUnits();
