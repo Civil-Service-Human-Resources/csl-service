a3b583375524d20f9a51416961340c137d15611d
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2025-06-04 14:30:27 +0100

    LC-3465 - GET organisations formatted list

--- src/main/java/uk/gov/cabinetoffice/csl/domain/csrs/AgencyToken.java
+++ src/main/java/uk/gov/cabinetoffice/csl/domain/csrs/AgencyToken.java
@@ -1,15 +1,18 @@
 package uk.gov.cabinetoffice.csl.domain.csrs;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.Set;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class AgencyToken {
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AgencyToken implements Serializable {
     private Long id;
     private String token;
     private String uid;
--- src/main/java/uk/gov/cabinetoffice/csl/domain/csrs/Domain.java
+++ src/main/java/uk/gov/cabinetoffice/csl/domain/csrs/Domain.java
@@ -1,15 +1,18 @@
 package uk.gov.cabinetoffice.csl.domain.csrs;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Domain {
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Domain implements Serializable {
     Long id;
     String domain;
     LocalDateTime createdTimestamp;
