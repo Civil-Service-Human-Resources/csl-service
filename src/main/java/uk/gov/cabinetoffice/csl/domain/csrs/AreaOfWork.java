62aa675033e9679a9a2e0bb7052c65f29dc00c0b
Jamie <jamie.lovell@cabinetofice.gov.uk>
2025-06-25 16:49:14 +0100

    Serializable area of work

--- src/main/java/uk/gov/cabinetoffice/csl/domain/csrs/AreaOfWork.java
+++ src/main/java/uk/gov/cabinetoffice/csl/domain/csrs/AreaOfWork.java
@@ -5,13 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AreaOfWork {
+public class AreaOfWork implements Serializable {
     private Long id;
     private String name;
     private List<AreaOfWork> children = new ArrayList<>();
